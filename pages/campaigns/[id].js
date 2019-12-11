d12b43fc68c1fb5d552b106dfac527553a7c5051
Zac Holland <zacharyholland@gmail.com>
2019-12-10 17:59:11 -0800

    can now update campaigns

--- pages/campaigns/[id].js
+++ pages/campaigns/[id].js
@@ -1,24 +1,54 @@
-import { useRouter } from "next/router";
+import { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
 import Layout from "../../components/Layouts/LayoutDrawer";
 
-const Campaign = ({ campaign }) => {
-  const router = useRouter();
+const Campaign = props => {
+  const [campaign, setCampaign] = useState(props.campaign);
+
+  const saveCampaign = async () => {
+    await fetch(`http://localhost:3001/v1/campaigns/${campaign.id}`, {
+      method: "PUT",
+      mode: "cors",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(campaign)
+    });
+  };
 
   return (
     <Layout>
-      <h1>{router.query.id}</h1>
-      <p>Name: {campaign.name}</p>
+      <h1>{campaign.name}</h1>
       <p>
         Pattern: Column {campaign.columnName} = {campaign.columnValue}
         <br />
-        CostPerMonth: {campaign.costPerMonth}
+        CostPerMonth:
+        <TextField
+          id="filled-number"
+          label="Number"
+          type="number"
+          InputLabelProps={{
+            shrink: true
+          }}
+          value={campaign.costPerMonth}
+          variant="outlined"
+          onChange={event => {
+            console.log(campaign);
+            campaign.costPerMonth = parseInt(event.target.value);
+            setCampaign({ ...campaign });
+          }}
+        />
       </p>
+      <Button variant="contained" color="primary" onClick={saveCampaign}>
+        Save
+      </Button>
     </Layout>
   );
 };
 
-Campaign.getInitialProps = async function() {
-  let res = await fetch("http://localhost:3001/v1/campaigns/1");
+Campaign.getInitialProps = async function({ query }) {
+  let res = await fetch(`http://localhost:3001/v1/campaigns/${query.id}`);
   const campaign = await res.json();
 
   return { campaign };
