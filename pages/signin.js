cb38fe3eb3a4a6e84c126bacfe2470458e096d4f
Zac Holland <zachary@pubnub.com>
2020-02-13 09:13:57 -0800

    cleaned imports

--- pages/signin.js
+++ pages/signin.js
@@ -1,21 +1,20 @@
 import React from "react";
 import Link from "next/link";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-// import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
+import {
+  Avatar,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Typography,
+  withStyles,
+  Container
+} from "@material-ui/core";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core";
-import Container from "@material-ui/core/Container";
 import { observer, inject } from "mobx-react";
 
 import Head from "../components/head";
-import LayoutVanilla from "../components/Layouts/LayoutVanilla";
 import Router from "next/router";
 
 const styles = theme => ({
