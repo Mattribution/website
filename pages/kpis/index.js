851b8dc451868bd000c5eea12162baa838dd6c0c
Zac Holland <zachary@pubnub.com>
2020-03-02 15:07:59 -0800

    moved components out of index

--- pages/kpis/index.js
+++ pages/kpis/index.js
@@ -6,128 +6,14 @@ import {
   Select,
   Typography
 } from "@material-ui/core";
-import { ResponsiveLine } from "@nivo/line";
-import { ResponsivePie } from "@nivo/pie";
 import Link from "next/link";
 import React, { useState } from "react";
+
+import { useFetchUser } from "../../lib/user";
+import KpiPieChart from '../../components/kpiPieChart';
+import KpiTimeDataLine from '../../components/kpiTimeDataLine';
 import Layout from "../../components/layoutDrawer";
 import useApi from "../../lib/use-api";
-import { useFetchUser } from "../../lib/user";
-
-function KpiTimeDataLine({ data }) {
-  return (
-    <ResponsiveLine
-      data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-		xScale={{ type: "time", format: '%Y-%m-%d', precision: 'day', stacked: false }}
-		xFormat="time:%Y-%m-%d"
-      yScale={{
-        type: "linear",
-        stacked: false
-      }}
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        orient: "bottom",
-		format: '%b %d',
-		tickValues: 'every 2 days',
-        legendOffset: 36,
-		legend: 'Days',
-        legendPosition: "middle"
-      }}
-      axisLeft={{
-        orient: "left",
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "count",
-        legendOffset: -40,
-        legendPosition: "middle"
-      }}
-      colors={{ scheme: "nivo" }}
-      pointSize={10}
-      pointColor={{ theme: "background" }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: "serieColor" }}
-      pointLabel="y"
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-        {
-          anchor: "bottom-right",
-          direction: "column",
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: "left-to-right",
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: "circle",
-          symbolBorderColor: "rgba(0, 0, 0, .5)",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemBackground: "rgba(0, 0, 0, .03)",
-                itemOpacity: 1
-              }
-            }
-          ]
-        }
-      ]}
-    />
-  );
-}
-function KpiPieChart({ data }) {
-  return (
-    <ResponsivePie
-      data={data}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-      innerRadius={0.5}
-      padAngle={0.7}
-      cornerRadius={3}
-      colors={{ scheme: "nivo" }}
-      borderWidth={1}
-      borderColor={{ from: "color", modifiers: [["darker", 0.2]] }}
-      radialLabelsSkipAngle={10}
-      radialLabelsTextXOffset={6}
-      radialLabelsTextColor="#333333"
-      radialLabelsLinkOffset={0}
-      radialLabelsLinkDiagonalLength={16}
-      radialLabelsLinkHorizontalLength={24}
-      radialLabelsLinkStrokeWidth={1}
-      radialLabelsLinkColor={{ from: "color" }}
-      slicesLabelsSkipAngle={10}
-      slicesLabelsTextColor="#333333"
-      animate={true}
-      motionStiffness={90}
-      motionDamping={15}
-      defs={[
-        {
-          id: "dots",
-          type: "patternDots",
-          background: "inherit",
-          color: "rgba(255, 255, 255, 0.3)",
-          size: 4,
-          padding: 1,
-          stagger: true
-        },
-        {
-          id: "lines",
-          type: "patternLines",
-          background: "inherit",
-          color: "rgba(255, 255, 255, 0.3)",
-          rotation: -45,
-          lineWidth: 6,
-          spacing: 10
-        }
-      ]}
-    />
-  );
-}
 
 function applyFirstTouch(aggregateData) {
   const pieData = {};
@@ -167,7 +53,7 @@ function applyFirstTouch(aggregateData) {
   }
 
   // ---------------------------
-  // --- Hanlde time series data
+  // --- Handle time series data
   // ---------------------------
   // Convert counts to totals
   for (var value of Object.keys(timeData)) {
